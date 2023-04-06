1b5097456b6160f7f3b7429911a3c7020b7a8c09
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-04-06 04:54:19 -0400

    Deleting commented code.

--- predict.py
+++ predict.py
@@ -106,56 +106,6 @@ def detect_face(
         save_face_chips(images, image_path, detected_images_output_dir)
 
 
-"""
-def detect_face(
-    image_paths, detected_images_output_dir, default_max_size=800, size=300, padding=0.25
-):
-    logger = logging.getLogger(__name__)
-    cnn_face_detector = dlib.cnn_face_detection_model_v1(
-        "dlib_models/mmod_human_face_detector.dat"
-    )
-    sp = dlib.shape_predictor("dlib_models/shape_predictor_5_face_landmarks.dat")
-    for index, image_path in enumerate(image_paths):
-        if index % 1000 == 0:
-            logger.info("---%d/%d---" % (index, len(image_paths)))
-
-        img = dlib.load_rgb_image(image_path)
-
-        old_height, old_width, _ = img.shape
-
-        if old_width > old_height:
-            new_width, new_height = default_max_size, int(
-                default_max_size * old_height / old_width
-            )
-        else:
-            new_width, new_height = (
-                int(default_max_size * old_width / old_height),
-                default_max_size,
-            )
-        img = dlib.resize_image(img, rows=new_height, cols=new_width)
-
-        dets = cnn_face_detector(img, 1)
-        num_faces = len(dets)
-        if num_faces == 0:
-            logger.info("Sorry, there were no faces found in '{}'".format(image_path))
-            continue
-        # Find the 5 face landmarks we need to do the alignment.
-        faces = dlib.full_object_detections()
-        for detection in dets:
-            rect = detection.rect
-            faces.append(sp(img, rect))
-        images = dlib.get_face_chips(img, faces, size=size, padding=padding)
-        for idx, image in enumerate(images):
-            img_name = image_path.split("/")[-1]
-            path_sp = img_name.split(".")
-            face_name = os.path.join(
-                detected_images_output_dir,
-                path_sp[0] + "_" + "face" + str(idx) + "." + path_sp[-1],
-            )
-            dlib.save_image(image, face_name)
-"""
-
-
 def predidct_age_gender_race(save_prediction_at, imgs_path="cropped_faces/"):
     logger = logging.getLogger(__name__)
     img_names = [os.path.join(imgs_path, x) for x in os.listdir(imgs_path)]
